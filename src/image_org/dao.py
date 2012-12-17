9366bb61a93ae2fd0ee67a47e25f2e49851e5cab
Matthias Schuetz <mschuetz@gmail.com>
2012-12-18 00:41:47 +0100

    check_store_key and unique_id did not match, fixed, added test

--- src/image_org/dao.py
+++ src/image_org/dao.py
@@ -9,7 +9,7 @@ class InvalidStoreKey(Exception):
     pass
 
 def check_store_key(store_key):    
-    if not re.match("^[0-9a-zA-Z_]+$", store_key):
+    if not re.match("^[0-9a-zA-Z_\-]+$", store_key):
         raise InvalidStoreKey()
 
 class ImageDao:
--- test_src/test_store.py
+++ test_src/test_store.py
@@ -1,6 +1,8 @@
 import unittest
 from StringIO import StringIO
 from image_org.store import LocalStore
+from image_org.store import unique_id
+from image_org.dao import check_store_key
 
 class TestLocalStore(unittest.TestCase):
     def setUp(self):
@@ -10,6 +12,11 @@ class TestLocalStore(unittest.TestCase):
         store_key = self.store.save(StringIO("foo"))
         with open(self.store.path(store_key)) as f:
             self.assertEquals("foo", f.read())
+
+    def test_store_keys_are_ok_for_dao(self):
+        for _ in xrange(100):
+            store_key = unique_id()
+            check_store_key(store_key)
 
 if __name__ == '__main__':
     unittest.main()
