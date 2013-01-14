d4e3ebf658afda779022bc419f8ab35ab7245ac7
Matthias Schuetz <mschuetz@gmail.com>
2013-01-14 01:02:40 +0100

    added some web smoke tests

---
+++ test_src/test_web.py
@@ -0,0 +1,54 @@
+import time
+import os
+import tempfile
+import json
+
+config = {
+    "language": "de",
+    "store": "local",
+    "session_secret": "asdf",
+    "localstore": {"basepath": tempfile.mkdtemp()},
+    "elasticsearch": {
+        "rawes": {
+            "url": "localhost:9200",
+            "timeout": 10
+        },
+        "indexname": "test_dao%s" % (time.time())
+    }
+}
+config_fn = tempfile.mktemp()
+with open(config_fn, 'w') as f:
+    json.dump(config, f)
+
+os.environ['TAMARAW_CONFIG'] = config_fn
+import tamaraw
+import unittest
+
+class TamarawTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.app = tamaraw.app.test_client()
+        tamaraw.user_dao.create_user('admin', 'asdf')
+
+    def test_smoke(self):
+        rv = self.app.get('/')
+        self.assertEquals('200 OK', rv.status)
+        tamaraw.image_dao.create('asdf', 'TEST', 'foo.jpg', prop_title='test title')
+        rv = self.app.get('/recent/')
+        self.assertEquals('200 OK', rv.status)
+        rv = self.app.get('/recent/o1')
+        self.assertEquals('200 OK', rv.status)
+        rv = self.app.get('/image/TEST')
+        self.assertEquals('200 OK', rv.status)
+        assert 'test title' in rv.data
+
+    def test_login(self):
+        # currently the login controller redirects to the referrer which is a full url and not supported by the flask testclient
+        rv = self.app.post('/login', data={'username': 'admin', 'password': 'asdf'})
+        #                   follow_redirects=True)
+        self.assertEquals('302 FOUND', rv.status)
+        # assert 'login succesful' in rv.data
+        
+
+if __name__ == '__main__':
+    unittest.main()
