93592a61753872cacb5b23a1d10208ba5e4a7d2a
Matthias Schuetz <mschuetz@gmail.com>
2013-02-17 15:47:09 +0100

    only run in debug mode if parameter given

--- bin/run.py
+++ bin/run.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python
 
 from tamaraw import app
-app.run(debug=True)
+import sys
+if len(sys.argv) > 1 and sys.argv[1].lower() == 'debug':
+    print >> sys.stderr, 'starting in debug mode'
+    app.run(debug=True)
+else:
+    app.run(debug=False)
